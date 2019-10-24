e30078096116aa12403d49469d1c4a8371789b9c
knatarasan <kannappan_natarasan@yahoo.com>
2019-10-24 13:34:10 -0700

    Added Login Controller and works as java application

--- src/main/java/com/bookshelf/conroller/LoginController.java
+++ src/main/java/com/bookshelf/conroller/LoginController.java
@@ -1,5 +1,15 @@
 package com.bookshelf.conroller;
 
-public class LoginController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+public class LoginController {
+	@ResponseBody
+	@RequestMapping("/home")
+	public String loginMessage() {
+		return "Vare va";
+	}
+	
 }
