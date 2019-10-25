91470bcaefb6d5a33cb3560c1468676afe91fd67
knatarasan <kannappan_natarasan@yahoo.com>
2019-10-25 15:20:08 -0700

    Added with jsp

--- src/main/java/com/bookshelf/conroller/LoginController.java
+++ src/main/java/com/bookshelf/conroller/LoginController.java
@@ -1,15 +1,20 @@
 package com.bookshelf.conroller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+//Step5 4th min
 @Controller
 public class LoginController {
-	@ResponseBody
-	@RequestMapping("/home")
-	public String loginMessage() {
-		return "Vare va";
+
+	@RequestMapping("/login")
+	public String loginMessage(@RequestParam String name) {
+		System.out.println(name);
+		return "login";
 	}
 	
 }
---
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>First Web Aplication</title>
+</head>
+
+<body>
+My first JSP ...!
+</body>
+</html>
